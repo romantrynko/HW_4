49e1e06a1fdc0f1840a1c9e356f0d451aebe5647
romantrynko <trynkoroman25@gmail.com>
2021-04-06 21:33:30 +0300

    reduce

--- main.js
+++ main.js
@@ -28,3 +28,77 @@
 
 
 
+// function functionWithCB (callBack) {
+//   console.log(callBack);
+
+//   callBack()
+// };
+
+// functionWithCB(function () {
+//   console.log('Call Back');
+// });
+
+// const arr = ['corona', 'blm', 'rain'];
+
+// const summer = (array) => {
+//   const [h, r] = array;
+//   console.log(h, r);
+// };
+
+// summer(['hello', 'Roman']);
+
+// const cbFunction = (value) => {
+//   const pro = prompt();
+
+//   value(pro);
+// };
+
+// cbFunction((param) => {
+//   document.write(param);
+// });
+
+// cbFunction(() => {
+//   console.log(22);
+// });
+
+
+// arr.sort((a, b) => {
+//   return a - b
+// });
+
+// let money = 0;
+
+// const clearHouse = (jobIsDone, cb) => {
+//   setTimeout(() => {
+//     if (jobIsDone) {
+//       money = 500;
+//       cb(money);
+//     } else {
+//       document.write('bad')
+//     }
+//   }, 2500)
+// };
+
+// clearHouse(true, (reward) => {
+//   console.log(reward);
+// });
+
+users = [
+  { name: 'Roman Trynko', age: 32 },
+  { name: 'Valia Dyatlov', age: 12 },
+  { name: 'Roman Vasyl', age: 45 },
+  { name: 'Pasha Loginov', age: 44 }
+];
+
+const nameToFind = 'ro'
+
+const filteredNames = users.reduce((prev, curr) => {
+  if (curr.name.toLowerCase().startsWith(nameToFind.toLocaleLowerCase()) && !prev.includes(curr.name)) {
+    prev.push(curr.name);
+  };
+
+  return prev;
+}, []);
+
+console.log(filteredNames);
+
