ecd82393ab379f2079ba1e13716a6902481f5f3e
romantrynko <trynkoroman25@gmail.com>
2021-04-06 18:15:17 +0300

    localStorage

--- main.js
+++ main.js
@@ -1,16 +1,23 @@
-const roman = {
+localStorage.setItem('1', 'value 1');
+
+const user = {
   name: 'Roman',
   age: 32,
-  cars: ['Honda', 'BMW'],
-  wife: {
-    name: 'Khrystyna',
-    age: 25,
-  }
-}
+  job: 'Senior JS developer'
+};
 
-const { cars: [, beha]} = roman;
+localStorage.setItem('user', user);
+localStorage.setItem('userJSON', JSON.stringify(user));
 
-console.log(beha);
+const jsonUser = localStorage.getItem('userJSON');
 
+const parsedUser = JSON.parse(jsonUser);
 
+console.log(jsonUser);
+console.log(parsedUser);
 
+localStorage.removeItem('user');
+
+console.log(localStorage.length);
+
+localStorage.clear();
