18ca9d899e1cc0432a1ee3a37b7d05156524e2aa
romantrynko <trynkoroman25@gmail.com>
2021-04-07 19:50:35 +0300

    fetch

--- main.js
+++ main.js
@@ -1,67 +1,18 @@
-let money = 0;
-let job = true;
+// https://api.privatbank.ua/p24api/pubinfo?json&exchange&coursid=5;
 
-const clearHouse = (jobDone) => {
-  return new Promise((resolve, reject) => {
-    setTimeout(() => {
-      if (jobDone) {
-        money = 500;
-
-        resolve(money);
-      } else {
-        reject('err')
-      }
-    }, 1000);
+fetch('https://api.privatbank.ua/p24api/pubinfo?json&exchange&coursid=5')
+  .then(value => {
+    return value.json();
   })
-};
+  .then(value => {
+    console.log(value);
 
-const buyJeans = (money) => {
-  return new Promise((resolve, reject) => {
-    setTimeout(() => {
-      if (money > 200) {
-        console.log('buy jeans');
-        money = money - 200;
-        resolve(money)
-      } else {
-        reject('no money')
-      }
-    }, 1000)
-  })
-};
+    value.forEach(({ base_ccy, buy, ccy, sale }) => {
+      document.write(`${base_ccy}, ${buy}, ${ccy}, ${sale}`);
+      document.write(`<br>`);
 
-const goEat = (money) => {
-  return new Promise((resolve, reject) => {
-    setTimeout(() => {
-      if (money > 100) {
-        console.log('go eat');
-        money = money - 50;
+    })
+  });
 
-        resolve(money);
-      } else {
-        reject('no money for food');
-      }
 
-    }, 1000);
-  })
-}
 
-const houseCleaner = async (job) => {
-  try {
-    const reward = await clearHouse(job);
-    console.log(reward);
-
-    const rest = await buyJeans(reward);
-    console.log(rest);
-
-    const food = await goEat(rest);
-    console.log(food);
-
-  } catch (error) {
-    throw new Error(error);
-  }
-};
-
-const btn = document.getElementById('btn');
-btn.onclick = (job) => {
-  return houseCleaner(!job);
-}
