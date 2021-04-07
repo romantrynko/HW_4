e5d275ca35dad88711ec0234723b43a99d32d911
romantrynko <trynkoroman25@gmail.com>
2021-04-07 19:25:28 +0300

    d

--- main.js
+++ main.js
@@ -1,91 +1,67 @@
-
-
-
-
-// function goToTurkey(covid, cb) {
-//   setTimeout(() => {
-//     if (covid) {
-//       cb('covid is present', null)
-//     } else {
-//       cb(null, 'everything is OK')
-//     }
-//   }, 2000)
-// };
-
-// goToTurkey(false, (err, data) => {
-//   if(err) {
-//     console.log(err, 'bad');
-//   } else {
-//     console.log('good', data);
-//   }
-// });
-
-
-
 let money = 0;
+let job = true;
 
-const clearHouse = (jobDone, cb) => {
-  setTimeout(() => {
-    if (jobDone) {
-      money = 500;
+const clearHouse = (jobDone) => {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      if (jobDone) {
+        money = 500;
 
-      cb(null, money);
-    } else {
-      cb('oops')
-    }
-  }, 2000);
+        resolve(money);
+      } else {
+        reject('err')
+      }
+    }, 1000);
+  })
 };
 
-const buyJeans = (money, cb) => {
-  setTimeout(() => {
-    if (money > 200) {
-      console.log('buy jeans');
-      money = money - 200;
-
-      cb(null, money)
-    } else {
-      console.log('no money');
-
-      cb('no', null)
-    }
-  }, 1000)
+const buyJeans = (money) => {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      if (money > 200) {
+        console.log('buy jeans');
+        money = money - 200;
+        resolve(money)
+      } else {
+        reject('no money')
+      }
+    }, 1000)
+  })
 };
 
-const goEat = (money, cb) => {
-  if (money > 100) {
-    console.log('go eat');
-    money = money - 50;
+const goEat = (money) => {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      if (money > 100) {
+        console.log('go eat');
+        money = money - 50;
 
-    cb(null, money)
-  } else {
-    console.log('no money');
-    
-    cb(err)
-  }
+        resolve(money);
+      } else {
+        reject('no money for food');
+      }
+
+    }, 1000);
+  })
 }
 
-clearHouse(true, (err, data) => {
-  if (err) {
-    document.write('something went wrong')
-  } else {
-    document.write('ok ' + data);
+const houseCleaner = async (job) => {
+  try {
+    const reward = await clearHouse(job);
+    console.log(reward);
 
-    buyJeans(data, (err, rest) => {
-      if (err) {
-        document.write('no money');
-      } else {
-        document.write('yes!');
+    const rest = await buyJeans(reward);
+    console.log(rest);
 
-        goEat(rest, (err, data) => {
-          if (err) {
-            document.write('no money for food');
-          } else {
-            document.write('eat your food')
-          }
-        })
-      }
-    })
+    const food = await goEat(rest);
+    console.log(food);
+
+  } catch (error) {
+    throw new Error(error);
   }
-});
+};
 
-console.log(money);
+const btn = document.getElementById('btn');
+btn.onclick = (job) => {
+  return houseCleaner(!job);
+}
