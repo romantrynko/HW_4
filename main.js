d5a6ef6bf6659dd824bf1430a4e33f591d2a2a6b
romantrynko <trynkoroman25@gmail.com>
2021-04-07 22:35:16 +0300

    call_apply_bind

--- main.js
+++ main.js
@@ -1,18 +1,54 @@
-// https://api.privatbank.ua/p24api/pubinfo?json&exchange&coursid=5;
 
-fetch('https://api.privatbank.ua/p24api/pubinfo?json&exchange&coursid=5')
-  .then(value => {
-    return value.json();
-  })
-  .then(value => {
-    console.log(value);
+const roman = {
+  name: 'Roman',
+  age: 32,
+  salary: 150000,
+  position: 'Senior'
+};
 
-    value.forEach(({ base_ccy, buy, ccy, sale }) => {
-      document.write(`${base_ccy}, ${buy}, ${ccy}, ${sale}`);
-      document.write(`<br>`);
+const nazar = {
+  name: 'Nazar',
+  age: 28,
+  salary: 150000,
+  position: 'Senior'
+};
 
-    })
-  });
+const veronika = {
+  name: 'Veronika',
+  age: 1,
+  salary: 120000,
+  position: 'Junior'
+};
+
+function sayHello() {
+  console.log(this.name + ' say hello');
+  console.log(this);
+};
+
+sayHello();
 
 
+sayHello.call(roman);
+sayHello.apply(nazar);
+let verSayHello = sayHello.bind(veronika);
 
+verSayHello();
+
+function changePayment(changeRate, newPos) {
+  this.salary += changeRate;
+  this.position = newPos;
+};
+
+changePayment.call(veronika, 100000, 'CEO');
+console.log(veronika);
+
+changePayment.apply(roman, [150000, 'CEO']);
+console.log(roman);
+
+let getMoneyForNazar = changePayment.bind(nazar, 200000, 'CEO');
+getMoneyForNazar();
+getMoneyForNazar();
+getMoneyForNazar();
+getMoneyForNazar();
+
+console.log(nazar);
